1c338678a4292547cd9b77b1ed8b99fad389efe1
ash0419 <ash0419@naver.com>
2022-12-28 15:38:17 +0900

    4ch complete

---
+++ src/routes/Profile.js
@@ -0,0 +1,19 @@
+import { authService } from "fbase"
+import { Navigate, useNavigate } from "react-router-dom"
+
+const Profile = () => {
+    const history = useNavigate()
+
+    const onLogOutClick = () => {
+        authService.signOut()
+        history("/")
+    }
+
+    return (
+        <>
+        <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    )
+}
+
+export default Profile
